f5376544610fa9cc5f85c87a85921b9aebfa13e9
Dongxun Yin <dongxuny@gmail.com>
2019-03-27 18:39:59 +0800

    Initial Commit

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -15,86 +15,78 @@ echo ""
 echo "---------------------------------------"
 echo ""
 
-# 2: Git clone
-echo "Step 2: Clone project HelloTencent from git ..."
-git clone https://github.com/donghun221/helloTencent.git
-
-echo ""
-echo "---------------------------------------"
-echo ""
-
-# 3: Move to project source root folder
-echo "Step 3: Move to project source root folder..."
+# 2: Move to project source root folder
+echo "Step 2: Move to project source root folder..."
 cd $PROJECT_NAME
 
 echo ""
 echo "---------------------------------------"
 echo ""
 
-# 4: Add dependencies
-echo "Step 4.1: get go-bindata"
+# 3: Add dependencies
+echo "Step 3.1: get go-bindata"
 go get -u github.com/jteeuwen/go-bindata/...
 
-echo "Step 4.2: get go-bindata-assetfs"
+echo "Step 3.2: get go-bindata-assetfs"
 go get github.com/elazarl/go-bindata-assetfs/...
 
-echo "Step 4.3: get protoc-gen-go"
+echo "Step 3.3: get protoc-gen-go"
 go get -u github.com/golang/protobuf/protoc-gen-go
 
-echo "Step 4.4: get protoc-gen-grpc-gateway"
+echo "Step 3.4: get protoc-gen-grpc-gateway"
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
 
-echo "Step 4.5: get protoc-gen-swagger"
+echo "Step 3.5: get protoc-gen-swagger"
 go get -u github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
-echo "Step 4.6: Add GOPATH to PATH"
+echo "Step 3.6: Add GOPATH to PATH"
 export PATH=$GOPATH/bin:$PATH
 
 echo ""
 echo "---------------------------------------"
 echo ""
 
-# 5: Compile proto file
-echo "Step 5.1: Compile proto file at api/*.proto ..."
+# 4: Compile proto file
+echo "Step 4.1: Compile proto file at api/*.proto ..."
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --go_out=plugins=grpc:. \
   api/*.proto
 
-echo "Step 5.2: Compile reverse proxy file at api/*.proto ..."
+echo "Step 4.2: Compile reverse proxy file at api/*.proto ..."
 protoc -I/usr/local/include -I. \
   -I$GOPATH/src \
   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis \
   --grpc-gateway_out=logtostderr=true:. \
   api/*.proto
 
-echo "Step 5.3: Compile swagger file at api/*.proto ..."
+echo "Step 4.3: Compile swagger file at api/*.proto ..."
 protoc -I/usr/local/include -I.   -I$GOPATH/src   -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis   --swagger_out=logtostderr=true:.   api/*.proto
 
-echo "Step 5.4: Compile swagger to go file at third-party/swagger-ui/* ..."
+echo "Step 4.4: Compile swagger to go file at third-party/swagger-ui/* ..."
 go-bindata --nocompress -pkg swagger -o examples/ui/data/swagger/datafile.go third-party/swagger-ui/...
 
 echo ""
 echo "---------------------------------------"
 echo ""
 
-# 6: Build main file
-echo "Step 6.1: Build main file at internal/main.go ..."
+# 5: Build main file
+echo "Step 5.1: Build main file at internal/main.go ..."
 go build -o HelloTencent internal/main.go
 
-echo "Step 6.2: Build common service client file at examples/common_service_client.go ..."
+echo "Step 5.2: Build common service client file at examples/common_service_client.go ..."
 go build -o CommonClient examples/common_service_client.go
 
-echo "Step 6.3: Build common service client file at examples/common_service_web.go ..."
+echo "Step 5.3: Build common service client file at examples/common_service_web.go ..."
 go build -o CommonWeb examples/common_service_web.go
 
 echo ""
 echo "---------------------------------------"
 echo ""
 
-# 7: Make target package
-echo "Step 5: Make target package ..."
+# 6: Make target package
+echo "Step 6: Make target package ..."
 rm -rf target
 
 mkdir target \
@@ -111,8 +103,8 @@ mkdir target/$PROJECT_NAME/bin \
       target/$PROJECT_NAME/api \
       target/$PROJECT_NAME/assets
 
-# 6: Copy related files to target folder
-echo "Step 6: Copy related files to target folder..."
+# 7: Copy related files to target folder
+echo "Step 7: Copy related files to target folder..."
 mv $PROJECT_NAME target/$PROJECT_NAME/bin/$PROJECT_NAME
 mv CommonClient target/$PROJECT_NAME/bin/CommonClient
 mv CommonWeb target/$PROJECT_NAME/bin/CommonWeb
