20325f03a623f0f15176e0f6cc859230842004c3
donghun221 <donghun221@gmail.com>
2019-03-28 21:41:45 +0800

    Initial Commit

--- build/jenkins_build.sh
+++ build/jenkins_build.sh
@@ -1,50 +1,44 @@
 #!/bin/bash
 
 ########## 1: Export GoPath
-echo "Step 1: Exporting GOPATH ..."
-export PATH=/var/jenkins_home/go/bin:$PATH
+# echo "Step 1: Exporting GOPATH ..."
+# export PATH=/var/jenkins_home/go/bin:$PATH
 
-echo ""
-echo "---------------------------------------"
-echo ""
+# echo "---------------------------------------"
 
 ########## 2: Declare to ENV 
-echo "Step 2: Exporting ENV ..."
-export PROJECT_DIR=/var/jenkins_home/tools/org.jenkinsci.plugins.golang.GolangInstallation/go_1.12/src
-export PROJECT_NAME=HelloTencent
-export PROJECT_PATH=$PROJECT_DIR/$PROJECT_NAME
+# echo "Step 2: Exporting ENV ..."
+# export PROJECT_DIR=/var/jenkins_home/tools/org.jenkinsci.plugins.golang.GolangInstallation/go_1.12/src
+# export PROJECT_NAME=HelloTencent
+# export PROJECT_PATH=$PROJECT_DIR/$PROJECT_NAME
 
-echo "PROJECT_DIR = $PROJECT_DIR"
-echo "PROJECT_NAME = $PROJECT_NAME"
-echo "PROJECT_PATH = $PROJECT_PATH"
+# echo "PROJECT_DIR = $PROJECT_DIR"
+# echo "PROJECT_NAME = $PROJECT_NAME"
+# echo "PROJECT_PATH = $PROJECT_PATH"
 
-echo ""
-echo "---------------------------------------"
-echo ""
+# echo "---------------------------------------"
 
 ########## 3: Move to PROJECT_DIR
 echo "Step 3: Move to project directory ..."
 cd $PROJECT_DIR
 
-echo ""
 echo "---------------------------------------"
-echo ""
 
 ########## 4: Clear PROJECT_PATH
-echo "Step 4: Clear project path ..."
-rm -rf $PROJECT_PATH
+echo "Step 4: Clear project path, Skip this in this demo version ..."
+# rm -rf $PROJECT_PATH
 
-echo ""
-echo "---------------------------------------"
-echo ""
+# echo ""
+# echo "---------------------------------------"
+# echo ""
 
 ########## 5: Git Clone
-echo "Step 5: Git Cloning ..."
-git clone https://github.com/donghun221/HelloTencent.git
+echo "Step 5: Git Cloning, Skip this in this demo version, use pull --rebase ..."
+# git clone https://github.com/donghun221/HelloTencent.git
+cd $PROJECT_PATH
+git pull --rebase
 
-echo ""
 echo "---------------------------------------"
-echo ""
 
 ########## 6: Compile proto file
 echo "Step 6.1: Compile proto file at api/*.proto ..."
@@ -67,9 +61,7 @@ echo "Step 6.3: Compile swagger file at api/*.proto ..."
 echo "Step 6.4: Compile swagger to go file at third-party/swagger-ui/* ..."
 /var/jenkins_home/go/bin/go-bindata --nocompress -pkg swagger -o examples/ui/data/swagger/datafile.go third-party/swagger-ui/...
 
-echo ""
 echo "---------------------------------------"
-echo ""
 
 
 ########## 7: Build main file
@@ -82,11 +74,9 @@ go build -o CommonClient examples/common_service_client.go
 echo "Step 7.3: Build common service client file at examples/common_service_web.go ..."
 go build -o CommonWeb examples/common_service_web.go
 
-echo ""
 echo "---------------------------------------"
-echo ""
 
-# 8: Make target package
+########## 8: Make target package
 echo "Step 6: Make target package ..."
 rm -rf target
 
@@ -103,8 +93,10 @@ mkdir target/$PROJECT_NAME/bin \
       target/$PROJECT_NAME/configs \
       target/$PROJECT_NAME/api \
       target/$PROJECT_NAME/assets
+      
+echo "---------------------------------------"
 
-# 7: Copy related files to target folder
+########## 9: Copy related files to target folder
 echo "Step 7: Copy related files to target folder..."
 mv $PROJECT_NAME target/$PROJECT_NAME/bin/$PROJECT_NAME
 mv CommonClient target/$PROJECT_NAME/bin/CommonClient
