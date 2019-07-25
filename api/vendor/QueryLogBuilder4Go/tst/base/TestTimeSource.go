b93b5e833a6c53b237ca135f12543a1535798316
Dongxun <donghun221@gmail.com>
2019-07-26 01:30:53 +0800

    asdf

--- api/vendor/QueryLogBuilder4Go/tst/base/TestTimeSource.go
+++
@@ -1,9 +0,0 @@
-package tst
-
-type TestTimeSource struct {
-	TimeToReturn int64
-}
-
-func (source *TestTimeSource) CurrentTimeMillis() int64 {
-	return source.TimeToReturn
-}
--- api/vendor/github.com/gogo/letmegrpc/install-godeps.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -xe
-mkdir -p $GOPATH/src/github.com/gogo
-git clone https://github.com/gogo/protobuf $GOPATH/src/github.com/gogo/protobuf
-go get google.golang.org/grpc
-go get golang.org/x/net/context
